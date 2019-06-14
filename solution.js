b137f15386d3f4970e895add6edb463d07e14137
collins kabali <51477747+ckabali@users.noreply.github.com>
2019-06-14 20:06:10 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,2 +1,16 @@
 // your solution should go here
 // for demo purposes use the console for any out you will need.
+B 
+
+ var recipe = {
+    'title': 'Mole',
+    'servings': 2,
+    'ingredients': ['cumin', 'cinnamon', 'cocoa']
+};
+
+console.log(recipe.title);
+console.log('Servings: ' + recipe.servings);
+console.log('Ingredients:');
+for (var i = 0; i < recipe.ingredients.length; i++) {
+    console.log(recipe.ingredients[i]);
+}
